b3b8f2266e7b993fed25beb65c21e25357ce90d7
Occ1pital <59239785+Occ1pital@users.noreply.github.com>
2020-01-26 10:04:13 -0600

    Update Radix.java

--- src/radix/Radix.java
+++ src/radix/Radix.java
@@ -1,3 +1,4 @@
+//Jovana Piña González Luis Gerardo Pacheco Castañeda
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
